321de582b7ba71cf7b46439c4de738a72d491d3b
meow <75662182+sakkios@users.noreply.github.com>
2021-06-10 16:17:53 +0800

    规定窗口大小

--- game.cpp
+++ game.cpp
@@ -270,6 +270,7 @@ int main()
 	while (1)  // 游戏循环执行
 	{
 		show();  // 显示画面
+		system("mode con cols=180 lines=50");  // 调整cmd窗口大小
 		HideCursor();  //  隐藏光标
 		updateWithoutInput();  // 与用户输入无关的更新
 		updateWithInput();  // 与用户输入有关的更新
